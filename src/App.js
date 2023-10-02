e637449b1391ee812851f38232473b27291178e7
muradgzade <gulverdizademurad@gmail.com>
2023-10-02 21:01:09 +0400

    incomplete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React  from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Sidebar from './shared/components/Sidebar/Sidebar';
 import Navbar from "./shared/components/Navbar/Navbar";
 import { AppContextProvider } from './shared/components/context/AppContext';
+import Checkbox from './shared/components/Checkbox/Checkbox';
+
+
 
 function App() {
+ 
   return (
     <Router>
       <AppContextProvider>
         <Navbar />
         <Sidebar />
+        <Checkbox/>
+    
       </AppContextProvider>
     </Router>
   );
---
+++ src/shared/components/Checkbox/Checkbox.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+
+
+const Checkbox = ({title}) => {
+  const [isChecked, setIsChecked] = useState(false);
+  const [isValid, setIsValid] = useState(true);
+    const [isRequired, setRequired] = useState(true);
+
+  const handleChange = () => {
+    setIsChecked(!isChecked);
+      setIsValid(true);
+      
+    };
+    
+ 
+
+  return (
+    <div>
+      <label>
+        [{isChecked ? 'click' : " "}]
+        {isRequired && <span style={{ color: "red" }}>*</span>}
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleChange}
+          disabled={!isValid}
+        />
+          </label>
+          
+         
+    </div>
+  );
+};
+
+export default Checkbox;
